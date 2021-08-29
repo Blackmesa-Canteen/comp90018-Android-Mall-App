1679fa613964e2e3d41a382af3c22a03c6058bb2
Blackmesa-Canteen <blackmesa_canteen@riseup.net>
2021-08-29 11:04:37 +0800

    added IM init and city json

--- app/src/main/java/com/comp90018/assignment2/App.java
+++ app/src/main/java/com/comp90018/assignment2/App.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import androidx.multidex.MultiDexApplication;
 
+import cn.jpush.im.android.api.JMessageClient;
+
 /**
  * Singleton, Represents the whole app
  * When the application starts, this part runs first
@@ -17,6 +19,15 @@ public class App extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         mContext = this;
+
+        /*
+         * The SDK initializes and specifies whether message logging roaming is enabled.
+         * After message roaming is enabled, when the user logs in between devices,
+         * the SDK will automatically synchronize the history messages of the current
+         * user to the local PC. After the synchronization is complete, the SDK will
+         * send a ConversationRefreshEvent event to notify the upper layer to refresh. 
+         */
+        JMessageClient.init(this, true);
     }
 
     /** Getter to get global context */
