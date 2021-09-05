e64e006dee5f9a678e85beca1cbe007683f14b1d
Blackmesa-Canteen <blackmesa_canteen@riseup.net>
2021-09-05 10:48:37 +0800

    Merge remote-tracking branch 'origin/home_page' into feature-search-activity

---
+++ app/src/main/java/com/comp90018/assignment2/utils/view/OvalImageView.java
@@ -0,0 +1,40 @@
+package com.comp90018.assignment2.utils.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class OvalImageView extends androidx.appcompat.widget.AppCompatImageView {
+    /* The radius of the rounded corner is the top left xy radius, top right, bottom right, bottom left */
+    private float[] rids = {20.0f, 20.0f, 20.0f, 20.0f, 20.0f, 20.0f, 20.0f, 20.0f,};
+
+
+    public OvalImageView(Context context) {
+        super(context);
+    }
+
+
+    public OvalImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public OvalImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        Path path = new Path();
+        int w = this.getWidth();
+        int h = this.getHeight();
+        /* Adds a rounded rectangle to the path. The Radii array defines the x,y
+        * radii of the four rounded corners of a rounded rectangle. The length of radii must be 8
+         */
+        path.addRoundRect(new RectF(0, 0, w, h), rids, Path.Direction.CW);
+        canvas.clipPath(path);
+        super.onDraw(canvas);
+    }
+}
