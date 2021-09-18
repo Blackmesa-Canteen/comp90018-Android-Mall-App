4f4b923fd24251c0ee0d703c8ef19449808b00dc
jzzh4 <jzzh4@student.unimelb.edu.au>
2021-09-19 01:35:37 +0800

    need to polish the xml file
    retrieve data from firebase is finished, but need to polish the xml file

--- app/src/main/java/com/comp90018/assignment2/modules/product/ProductDetailActivity.java
+++ app/src/main/java/com/comp90018/assignment2/modules/product/ProductDetailActivity.java
@@ -1,6 +1,8 @@
 package com.comp90018.assignment2.modules.product;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,25 +10,35 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.comp90018.assignment2.R;
 import com.comp90018.assignment2.databinding.ActivityProductDetailBinding;
 import com.comp90018.assignment2.dto.ProductDTO;
 import com.comp90018.assignment2.dto.UserDTO;
 import com.comp90018.assignment2.modules.search.activity.SearchProductActivity;
 import com.comp90018.assignment2.modules.search.activity.SearchResultActivity;
+import com.comp90018.assignment2.modules.search.adapter.SearchResultRvAdapter;
 import com.comp90018.assignment2.modules.users.authentication.activity.LoginActivity;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.List;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
     private ActivityProductDetailBinding binding;
+    private FirebaseStorage storage;
+    private ProductDetailAdapter productDetailAdapter;
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        storage = FirebaseStorage.getInstance();
 
         binding = ActivityProductDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -34,63 +46,80 @@ public class ProductDetailActivity extends AppCompatActivity {
         // retrieve bundle data from intent li xiaotian
         Intent intent = getIntent();
 
+        recyclerView=binding.lvPdtDetailImages;
         ProductDTO productDTO = (ProductDTO) intent.getParcelableExtra("productDTO");
         UserDTO userDTO = (UserDTO) intent.getParcelableExtra("userDTO");
+        productDetailAdapter = new ProductDetailAdapter(this, productDTO.getImage_address());
+
+       recyclerView.setAdapter(productDetailAdapter);
+// 2 columns grid
+        GridLayoutManager gvManager = new GridLayoutManager(this, 4);
+        recyclerView.setLayoutManager(gvManager);
 
 
-        /** go previous page */
-        binding.pdtBackBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+//        binding.pdtDetailUserNickname.setText(userDTO.getNickname());
+//        binding.tvPdtDetailPrice.setText(productDTO.getPrice().toString());
+        StorageReference imgReference = storage.getReferenceFromUrl(userDTO.getAvatar_address());
 
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Go Previous Page", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        /** go profile page */
-        binding.pdtDetailBtnProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ProductDetailActivity.this, LoginActivity.class);
-                startActivity(intent);
+        // query image with the reference
+        Glide.with(ProductDetailActivity.this)
+                .load(imgReference)
+                .into(binding.ivPdtDetailAvatar);
 
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Go Profile Page", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        /** add current product to my favourite */
-        binding.llFavouriteBtnGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Add to my favourite", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        /** comments on current product */
-        binding.llCommentBtnGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "comments on current product", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-        /** want current product */
-        binding.pdtDetailBtnWantThis.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "Click Want this", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
+//        /** go previous page */
+//        binding.pdtBackBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//
+//                Toast toast = Toast.makeText(getApplicationContext(),
+//                        "Go Previous Page", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+//        /** go profile page */
+//        binding.pdtDetailBtnProfile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(ProductDetailActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//
+//                Toast toast = Toast.makeText(getApplicationContext(),
+//                        "Go Profile Page", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+//        /** add current product to my favourite */
+//        binding.llFavouriteBtnGroup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Toast toast = Toast.makeText(getApplicationContext(),
+//                        "Add to my favourite", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+//        /** comments on current product */
+//        binding.llCommentBtnGroup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Toast toast = Toast.makeText(getApplicationContext(),
+//                        "comments on current product", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+//        /** want current product */
+//        binding.pdtDetailBtnWantThis.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Toast toast = Toast.makeText(getApplicationContext(),
+//                        "Click Want this", Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
 
     }
 }
--- app/src/main/java/com/comp90018/assignment2/modules/product/ProductDetailAdapter.java
+++ app/src/main/java/com/comp90018/assignment2/modules/product/ProductDetailAdapter.java
@@ -1,48 +1,100 @@
 package com.comp90018.assignment2.modules.product;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.comp90018.assignment2.R;
 import com.comp90018.assignment2.dto.ProductDTO;
 import com.comp90018.assignment2.dto.UserDTO;
+import com.comp90018.assignment2.modules.search.adapter.SearchResultRvAdapter;
+import com.comp90018.assignment2.utils.Constants;
+import com.comp90018.assignment2.utils.view.OvalImageView;
+import com.donkingliang.labels.LabelsView;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ProductDetailAdapter extends BaseAdapter {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProductDetailAdapter extends RecyclerView.Adapter {
 
     private Context context;
     private LayoutInflater layoutInflater;
-
-    private List<ProductDTO> productDTOList;
-    private Map<DocumentReference, UserDTO> userDTOMap;
-
+    private List<String> productDTOList;
     private FirebaseStorage storage;
 
+    public ProductDetailAdapter(Context ctx, List<String> productDTOList) {
 
-    @Override
-    public int getCount() {
+        this.context = ctx;
+        this.productDTOList = productDTOList;
+        storage = FirebaseStorage.getInstance();
+        layoutInflater = LayoutInflater.from(context);
+
+    }
+
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        // only one type of view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_images, parent, false);
+        return new ProductDetailAdapter.ItemProductInfoViewHolder(context, view);
+    }
+
+    public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
+        ProductDetailAdapter.ItemProductInfoViewHolder viewHolder = (ProductDetailAdapter.ItemProductInfoViewHolder) holder;
+        viewHolder.setData(productDTOList, position);
+    }
+
+    public int getItemCount() {
         return productDTOList.size();
 
     }
 
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
+    /**
+     * ViewHolder for items
+     */
+    private class ItemProductInfoViewHolder extends RecyclerView.ViewHolder {
 
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
+        private Context context;
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        private OvalImageView imgProductImage;
+
+
+        public ItemProductInfoViewHolder(Context context, View inflate) {
+            super(inflate);
+            this.context = context;
+
+            // bind views
+            imgProductImage = (OvalImageView) inflate.findViewById(R.id.item_image);
+
+        }
+            public void setData(List<String> productDTOList, final int position) {
+
+                StorageReference imgReference = storage.getReferenceFromUrl(productDTOList.get(position));
+
+            // query image with the reference
+            Glide.with(context)
+                    .load(imgReference)
+                    .into(imgProductImage);
+        }
     }
 }
+
