c23e1279763c0d0aabaa8e98ba88f8eb69c4f103
kkk-1518 <69903727+kkk-1518@users.noreply.github.com>
2021-09-24 22:59:33 +0800

    Update MeFragment.java

--- app/src/main/java/com/comp90018/assignment2/modules/users/me/fragment/MeFragment.java
+++ app/src/main/java/com/comp90018/assignment2/modules/users/me/fragment/MeFragment.java
@@ -1,19 +1,62 @@
 package com.comp90018.assignment2.modules.users.me.fragment;
 
+
+import static com.comp90018.assignment2.config.VideoDataSource.TAG;
+import static com.comp90018.assignment2.utils.Constants.USERS_COLLECTION;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
+
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 import com.comp90018.assignment2.R;
 import com.comp90018.assignment2.base.BaseFragment;
-import com.comp90018.assignment2.modules.users.me.activity.EditProfileActivity;
-import com.google.firebase.firestore.FirebaseFirestore;
 
+import com.comp90018.assignment2.db.service.UserService;
+import com.comp90018.assignment2.dto.UserDTO;
+import com.comp90018.assignment2.modules.users.authentication.activity.EditProfileActivity;
+import com.comp90018.assignment2.modules.users.authentication.activity.LoginActivity;
+import com.comp90018.assignment2.utils.Constants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import com.comp90018.assignment2.modules.users.authentication.activity.EditProfileActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
 /**
  *
- * @author you
+ * @author Zhonghui Jiang
  */
 public class MeFragment extends BaseFragment {
 
@@ -34,7 +77,17 @@ public class MeFragment extends BaseFragment {
     private TextView tv_feedback;
     private TextView tv_setting;
     private TextView tv_about;
-    FirebaseFirestore db;
+    private TextView tv_follower_number;
+    private TextView tv_following_number;
+
+    private Button tv_logout;
+
+
+    private TextView tv_nick_name;
+    private TextView tv_loginID;
+
+
+
 
 
 
@@ -60,35 +113,172 @@ public class MeFragment extends BaseFragment {
         tv_feedback = (TextView) view.findViewById(R.id.tv_feedback);
         tv_setting = (TextView) view.findViewById(R.id.tv_setting);
         tv_about = (TextView) view.findViewById(R.id.tv_about);
+        tv_logout=(Button) view.findViewById(R.id.tv_logout);
+        tv_follower_number=(TextView) view.findViewById(R.id.tv_follower_number);
+        tv_following_number=(TextView) view.findViewById(R.id.tv_following_number);
+
+
+        tv_nick_name = (TextView) view.findViewById(R.id.tv_nick_name);
+        tv_loginID =(TextView) view.findViewById(R.id.tv_loginID);
+
+        setListeners();
 
         return view;
-
-
-
-
-
-
-
-        
     }
+
+    private void setListeners(){
+        onClick onClick= new onClick();
+        ib_profile_arrow.setOnClickListener(onClick);
+        tv_profile.setOnClickListener(onClick);
+        tv_published.setOnClickListener(onClick);
+        tv_sold.setOnClickListener(onClick);
+        tv_purchased.setOnClickListener(onClick);
+        tv_update.setOnClickListener(onClick);
+        tv_logout.setOnClickListener(onClick);
+        tv_favorate.setOnClickListener(onClick);
+        tv_address.setOnClickListener(onClick);
+        tv_payment.setOnClickListener(onClick);
+        tv_feedback.setOnClickListener(onClick);
+        tv_setting.setOnClickListener(onClick);
+        tv_about.setOnClickListener(onClick);
+        tv_follower_number.setOnClickListener(onClick);
+        tv_following_number.setOnClickListener(onClick);
+
+
+    }
+
+
 
     public void onActivityCreated(Bundle savedInstanceStade) {
 
         super.onActivityCreated(savedInstanceStade);
-        ImageButton b1 = (ImageButton) getActivity().findViewById(R.id.ib_profile_arrow);
-        b1.setOnClickListener(new View.OnClickListener() {
+        tv_update = (TextView) getActivity().findViewById(R.id.tv_update);
+        tv_update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent ToEditProfileActivity = new Intent(activityContext, EditProfileActivity.class);
                 startActivity(ToEditProfileActivity);
             }
         });
+
+    }
+    //点击跳转activity
+    private class onClick implements View.OnClickListener {
+        @Override
+
+        public void onClick(View v) {
+
+            Intent intent = null;
+            switch (v.getId()){
+
+
+                case R.id.ib_profile_arrow:  //点击profile或者箭头跳转用户个人信息
+
+                case R.id.tv_profile :
+
+                    Toast.makeText(activityContext, "个人信息页面", Toast.LENGTH_SHORT).show();
+
+                    break;
+                case R.id.tv_published:
+
+                    Toast.makeText(activityContext, "商品发布详情", Toast.LENGTH_SHORT).show();
+
+                    break;
+                case R.id.tv_sold:
+                    Toast.makeText(activityContext, "已卖出商品", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.tv_purchased:
+                    Toast.makeText(activityContext, "已买到商品", Toast.LENGTH_SHORT).show();
+                    break;
+//                点击publish，sold，purchased 分别跳转相应界面
+                case R.id.tv_favorate:
+                    Toast.makeText(activityContext, "收藏商品", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.tv_address:
+                    Toast.makeText(activityContext, "送货地址", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.tv_payment:
+                    Toast.makeText(activityContext, "支付信息", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.tv_setting:
+                    Toast.makeText(activityContext, "设置", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.tv_feedback:
+                    Toast.makeText(activityContext, "反馈", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.tv_about:
+                    Toast.makeText(activityContext, "关于", Toast.LENGTH_SHORT).show();
+                    break;
+
+//                case R.id.tv_update:
+//                    intent = new Intent(activityContext, EditProfileActivity.class);
+//                    startActivity(intent);
+//
+//                    break;
+                case R.id.tv_logout:
+                    intent = new Intent(activityContext, LoginActivity.class);
+                    startActivity(intent);
+
+
+                    break;
+            }
+
+
+        }
+
     }
 
     @Override
     public void loadData() {
         /* 实际上，这个方法会从网上请求数据，然后你要把数据在这个方法里装到对应的view里 */
 //        imageView.setImageResource(R.drawable.profile);
+        FirebaseAuth firebaseAuth;
+        FirebaseFirestore db;
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+
+        String loginID = firebaseAuth.getCurrentUser().getEmail();
+        String userID=firebaseAuth.getCurrentUser().getUid();
+        Uri avatar=firebaseAuth.getCurrentUser().getPhotoUrl();
+
+
+        DocumentReference userReference = db.collection(USERS_COLLECTION).document(userID);
+        userReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                UserDTO userDTO = documentSnapshot.toObject(UserDTO.class);
+                String follower = String.valueOf(userDTO.getFollower_refs().size());
+                String following = String.valueOf(userDTO.getFollowing_refs().size());
+
+                tv_loginID.setText("Login ID: " + loginID);
+
+                ci_user.setImageURI(avatar);
+                tv_nick_name.setText(userDTO.getNickname());
+                tv_follower_number.setText(follower);
+                tv_following_number.setText(following);
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
