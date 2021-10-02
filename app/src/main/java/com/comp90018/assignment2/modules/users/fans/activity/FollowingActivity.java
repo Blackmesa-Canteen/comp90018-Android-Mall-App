a9494b7b69a160a1cbdf5d88909f6ca362805282
Blackmesa-Canteen <blackmesa_canteen@riseup.net>
2021-10-02 10:59:42 +0800

    added xml layouts

---
+++ app/src/main/java/com/comp90018/assignment2/modules/users/fans/activity/FollowingActivity.java
@@ -0,0 +1,16 @@
+package com.comp90018.assignment2.modules.users.fans.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.comp90018.assignment2.R;
+
+public class FollowingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_following);
+    }
+}
---
+++ app/src/main/java/com/comp90018/assignment2/modules/users/favorite/activity/FavoriteProductActivity.java
@@ -0,0 +1,16 @@
+package com.comp90018.assignment2.modules.users.favorite.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.comp90018.assignment2.R;
+
+public class FavoriteProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_product);
+    }
+}
