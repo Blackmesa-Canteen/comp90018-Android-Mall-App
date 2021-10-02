e5c65647f0e227f5bb0063d90f3a4943ad154325
lengary1110 <ziyuanx1@student.unimelb.edu.au>
2021-10-02 18:16:19 +0800

    publish: add location

--- app/src/main/java/com/comp90018/assignment2/modules/publish/activity/PublishProductActivity.java
+++ app/src/main/java/com/comp90018/assignment2/modules/publish/activity/PublishProductActivity.java
@@ -1,8 +1,15 @@
 package com.comp90018.assignment2.modules.publish.activity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,7 +20,6 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,13 +32,12 @@ import com.comp90018.assignment2.databinding.ActivityPublishProductBinding;
 import com.comp90018.assignment2.dto.CategoryDTO;
 import com.comp90018.assignment2.dto.ProductDTO;
 import com.comp90018.assignment2.dto.SubCategoryDTO;
+
+import com.comp90018.assignment2.modules.location.utils.LocationHelper;
 import com.comp90018.assignment2.modules.publish.adapter.PictureCollectionAdapter;
-import com.comp90018.assignment2.modules.users.authentication.activity.RegisterActivity;
 import com.comp90018.assignment2.utils.Constants;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.firebase.geofire.GeoFireUtils;
+import com.firebase.geofire.GeoLocation;
 import com.google.firebase.Timestamp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -41,14 +46,10 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.config.PictureMimeType;
 import com.luck.picture.lib.entity.LocalMedia;
-
-
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -59,13 +60,15 @@ import java.util.Map;
 /**
  * @author Ziyuan Xu
  */
-public class PublishProductActivity extends AppCompatActivity{
+public class PublishProductActivity extends AppCompatActivity {
     private final static String TAG = "PublishProductActivity";
     private ActivityPublishProductBinding binding;
     private FirebaseStorage firebaseStorage;
     private FirebaseAuth firebaseAuth;
     FirebaseFirestore db;
     private Spinner categorySpinner;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     private Spinner subcategorySpinner;
     private List<CategoryDTO> categories = new ArrayList<>();
     private Map<CategoryDTO, List<SubCategoryDTO>> categoryBundles = new HashMap<>();
@@ -75,7 +78,13 @@ public class PublishProductActivity extends AppCompatActivity{
     private ImageView pf_add;
     private RecyclerView pf_collection;
     private ArrayList<String> images = new ArrayList<>();
+    private Double lng;
+    private Double lat;
+    private String hash;
+    private String location_text;
 
+    @SuppressLint("MissingPermission")
+    // TODO: MissingPermission? exception
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,7 +93,9 @@ public class PublishProductActivity extends AppCompatActivity{
         // init view binding
         binding = ActivityPublishProductBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
         // attach to layout file
         setContentView(view);
         pf_collection = view.findViewById(R.id.pf_collection);
@@ -249,13 +260,13 @@ public class PublishProductActivity extends AppCompatActivity{
             }
 
             // update images
-            if (images.size() == 0 || images.get(1) == null) {
-                new AlertDialog.Builder(PublishProductActivity.this).setMessage("One picture required").setPositiveButton("ok", null).show();
+            if (images == null || images.size() == 0) {
+                new AlertDialog.Builder(PublishProductActivity.this).setMessage("One uploaded picture required").setPositiveButton("ok", null).show();
                 return;
             } else {
                 FirebaseUser currentUser = firebaseAuth.getCurrentUser();
                 DocumentReference userReference = db.collection(Constants.USERS_COLLECTION).document(currentUser.getUid());
-                // TODO(add geo): get geo info - if not: set default geo info
+                // TODO: location_coordinate?
                 ProductDTO newProductDTO = ProductDTO.builder()
                         .owner_ref(userReference)
                         .price(Double.parseDouble(price))
@@ -271,10 +282,12 @@ public class PublishProductActivity extends AppCompatActivity{
                         .favorite_number(0)
                         .currency(0)
                         .status(0)
-                        .geo_hash(null)
-                        .lat(null)
-                        .lng(null)
-                        .location_text(null)
+                        .geo_hash(hash)
+                        .lat(lat)
+                        .lng(lng)
+                        .star_number(0.0)
+                        .location_coordinate(null)
+                        .location_text(location_text)
                         .build();
                 publish(newProductDTO);
             }
@@ -311,11 +324,10 @@ public class PublishProductActivity extends AppCompatActivity{
                        db.collection(Constants.PRODUCT_COLLECTION)
                                .document(documentReference.getId())
                                .update("id", documentReference.getId());
-                               // TODO: after publish successfully, go to published page
-                               // TODO: after publish successfully add sound
                       // finish the activity
                       publishProgressDialog.dismiss();
                       finish();
+                   // TODO(goto): after publish successfully, go to product detailed page
                }).addOnFailureListener(e -> {
                       publishProgressDialog.dismiss();
                       Log.w(TAG, "Create product:failed", e);
@@ -325,4 +337,15 @@ public class PublishProductActivity extends AppCompatActivity{
                               .setPositiveButton("Ok", null).show();
                });
     }
+    public class MyLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location loc) {
+            lng =  loc.getLongitude();
+            lat = loc.getLatitude();
+            hash = GeoFireUtils.getGeoHashForLocation(new GeoLocation(lat, lng));
+            LocationHelper.getTextAddressWithCoordinate(
+                    lat, lng, locationBean -> location_text = locationBean.getTextAddress()
+            );
+            // TODO(goto): 处理失败情况，设置默认值
+        }}
 }
